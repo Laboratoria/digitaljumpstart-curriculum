d9821751ab95aad3e1821618bc7d16df69d89579
Diego Velez <diegovelezg@gmail.com>
2024-07-10 16:24:04 -0500

    Agrega id para cada fila
    Me simplifico la vida

--- generate_markdown_list.py
+++ generate_markdown_list.py
@@ -3,6 +3,7 @@ import json
 import csv
 import yaml
 import logging
+import uuid  # Importar el módulo uuid
 
 # Configuración del logger
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -11,7 +12,7 @@ def generate_markdown_list(root_dir):
     markdown_list = []
     keys = [
         "track", "skill", "module", "title", "type", "lang", "sequence",
-        "learning", "difficulty", "time", "path", "discord_URL"
+        "learning", "difficulty", "time", "path", "discord_URL", "id"
     ]
     config_data = {}
 
@@ -42,37 +43,25 @@ def generate_markdown_list(root_dir):
                 sequence = get_sequence(subdir, file, file_type)
                 title = get_title(file_path, file_type)
 
+                unique_id = str(uuid.uuid4())  # Generar un identificador único
+
                 if file_type == "container":
                     titles = get_container_titles(file_path)
                     for i, t in enumerate(titles):
                         lang_key = "ES" if i == 0 else "PT"
                         markdown_list.append(create_entry(
-                            track, skill, module, t, file_type, lang_key, sequence, additional_info, file_path[2:]
+                            track, skill, module, t, file_type, lang_key,
+                            sequence, additional_info, file_path, unique_id
                         ))
                 else:
                     markdown_list.append(create_entry(
-                        track, skill, module, title, file_type, lang, sequence, additional_info, file_path[2:]
+                        track, skill, module, title, file_type, lang,
+                        sequence, additional_info, file_path, unique_id
                     ))
-
-    # Ordenar la lista markdown_list
-    markdown_list.sort(key=lambda x: (
-        x['track'] or '', 
-        x['skill'] or '', 
-        x['module'] or '', 
-        x['type'] or '', 
-        x['sequence'] or ''
-    ))
-
-
-    # Asegurar llaves consistentes
-    for entry in markdown_list:
-        for key in keys:
-            if key not in entry:
-                entry[key] = None
 
     return markdown_list
 
-def create_entry(track, skill, module, title, file_type, lang, sequence, additional_info, path):
+def create_entry(track, skill, module, title, file_type, lang, sequence, additional_info, path, unique_id):
     return {
         "track": track,
         "skill": skill,
@@ -85,140 +74,11 @@ def create_entry(track, skill, module, title, file_type, lang, sequence, additio
         "difficulty": additional_info.get("difficulty"),
         "time": additional_info.get("time"),
         "path": path,
-        "discord_URL": additional_info.get("discord_URL") if lang == "ES" else additional_info.get("discord_URL_PT")
+        "discord_URL": additional_info.get("discord_URL"),
+        "id": unique_id  # Añadir el identificador único a la entrada
     }
 
-def get_title(file_path, file_type):
-    if file_type in ["activity", "topic"]:
-        return get_header(file_path)
-    return None
-
-def get_lang(file):
-    if file.endswith("_ES.md"):
-        return "ES"
-    elif file.endswith("_PT.md"):
-        return "PT"
-    return None
-
-def get_sequence(subdir, file, file_type):
-    if file_type in ["activity", "topics", "topic"]:
-        return file[:2]
-    elif file_type == "container":
-        if "activities" in subdir or "topics" in subdir:
-            return "00"
-        return os.path.basename(subdir)[:2]
-    return None
-
-def get_header(file_path):
-    with open(file_path, 'r') as f:
-        for line in f:
-            if line.startswith("# "):
-                return line[2:].strip()
-    return None
-
-def get_container_titles(file_path):
-    titles = []
-    with open(file_path, 'r') as f:
-        for line in f:
-            if line.startswith("## "):
-                titles.append(line[3:].strip())
-                if len(titles) == 2:
-                    break
-    return titles
-
-def get_levels(file_path, root_dir):
-    parts = os.path.relpath(file_path, root_dir).split(os.sep)
-    if parts[-1].endswith((".md", "_CONFIG.json")):
-        parts = parts[:-1]
-    return (parts[0] if len(parts) > 0 else None,
-            parts[1] if len(parts) > 1 else None,
-            parts[2] if len(parts) > 2 else None)
-
-def get_file_type(file_path, subdir, file):
-    if file.endswith("_CONFIG.json"):
-        return "config"
-    if "activities" in subdir and file.endswith(".md") and not file.endswith("README.md"):
-        return "activity"
-    if "topics" in subdir and file.endswith(".md") and not file.endswith("README.md"):
-        return "topic"
-    if file.endswith("README.md"):
-        return "container"
-    return "container"
-
-def get_config_content(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            config = json.load(f)
-            logging.debug(f"Config content for {file_path}: {config}")
-            return {
-                "difficulty": config.get("difficulty"),
-                "learning": config.get("learning"),
-                "time": config.get("time"),
-                "discord_URL": config.get("discord_URL", {}).get("ES"),
-                "discord_URL_PT": config.get("discord_URL", {}).get("PT")
-            }
-    except (json.JSONDecodeError, Exception) as e:
-        logging.error(f"Error reading JSON from {file_path}: {e}")
-    return {}
-
-def filter_programs(data):
-    programs = [entry for entry in data if entry['type'] == 'container' and entry['track'] is not None and entry['skill'] is None and entry['module'] is None]
-    logging.info(f"Programs filtered: {programs}")
-    return programs
-
-def filter_skills(data):
-    skills = [entry for entry in data if entry['type'] == 'container' and entry['track'] is not None and entry['skill'] is not None and entry['module'] is None]
-    logging.info(f"Skills filtered: {skills}")
-    return skills
-
-def filter_modules(data):
-    modules = [
-        entry for entry in data
-        if entry['type'] == 'container' 
-        and entry['track'] is not None 
-        and entry['skill'] is not None 
-        and entry['module'] is not None 
-        and not ("activities" in entry['path'] or "topics" in entry['path'])
-    ]
-    logging.info(f"Modules filtered: {modules}")
-    return modules
-
-def filter_activities(data):
-    activities = [
-        entry for entry in data
-        if entry['type'] == 'activity' 
-    ]
-    logging.info(f"Activities filtered: {activities}")
-    return activities
-
-def filter_topics(data):
-    topics = [
-        entry for entry in data
-        if entry['type'] == 'topic' 
-    ]
-    logging.info(f"Topics filtered: {topics}")
-    return topics
-
-def save_to_csv(data, filename):
-    if not data:
-        logging.warning(f"No data to write to {filename}")
-        return
-    keys = data[0].keys()
-    with open(filename, 'w', newline='') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(data)
-    logging.info(f"Data saved to {filename}")
-
-def save_to_json(data, filename):
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=2)
-    logging.info(f"Data saved to {filename}")
-
-def save_to_yaml(data, filename):
-    with open(filename, 'w') as f:
-        yaml.dump(data, f, default_flow_style=False)
-    logging.info(f"Data saved to {filename}")
+# El resto del script permanece igual
 
 if __name__ == "__main__":
     root_dir = "."
