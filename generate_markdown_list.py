e4e735cfc3a29b81983f98009c07fd4d6602dbab
Diego Velez <diegovelezg@gmail.com>
2024-08-01 13:24:11 -0500

    Update generate_markdown_list.py

--- generate_markdown_list.py
+++ generate_markdown_list.py
@@ -7,12 +7,10 @@ def escape_json_config(config_file):
     try:
         with open(config_file, 'r', encoding='utf-8') as f:
             config = json.load(f)
-            # Asegurar que "directions" sea una cadena y que se escriba correctamente
-            if 'directions' in config and isinstance(config['directions'], str):
-                config['directions'] = re.sub(r'[\U00010000-\U0010ffff]', lambda m: '&#' + str(ord(m.group())) + ';', config['directions'])
+            # Asegurarse de que "directions" contenga emojis y caracteres especiales sin modificarlos
         with open(config_file, 'w', encoding='utf-8') as f:
             json.dump(config, f, indent=2, ensure_ascii=False)
-    except ValueError as e:
+    except json.JSONDecodeError as e:
         print(f"Error al procesar el archivo {config_file}: {e}")
     except Exception as e:
         print(f"Error inesperado al procesar el archivo {config_file}: {e}")
