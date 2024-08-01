d1e83667e29ca65c6a9d2d6449863f846fc8ca40
Diego Velez <diegovelezg@gmail.com>
2024-08-01 16:08:32 -0500

    Update generate_markdown_list.py

--- generate_markdown_list.py
+++ generate_markdown_list.py
@@ -6,10 +6,8 @@ import requests
 import logging
 
 def clean_control_characters(json_str):
-    # Reemplazar caracteres de escape incorrectos
     json_str = json_str.replace('\\_', '_')
-    # Eliminar caracteres de control y manejar caracteres de escape inválidos
-    json_str = re.sub(r'\\(?!["\\/bfnrtu])', r'\\\\', json_str)  # Escapar correctamente las barras invertidas
+    json_str = re.sub(r'\\(?!["\\/bfnrtu])', r'\\\\', json_str)
     return re.sub(r'[\x00-\x1F\x7F]', '', json_str)
 
 def escape_json_config(config_file):
@@ -32,7 +30,6 @@ def process_config_files(root_dir):
                 config_file = os.path.join(subdir, file)
                 escape_json_config(config_file)
 
-
 def generate_markdown_list(root_dir):
     markdown_list = []
     for subdir, _, files in os.walk(root_dir):
@@ -40,12 +37,11 @@ def generate_markdown_list(root_dir):
             if file.endswith(".md"):
                 file_path = os.path.join(subdir, file)
                 track, skill, module = get_levels(file_path, root_dir)
-                file_type = get_file_type(file_path, subdir, file)
+                file_type = get_file_type(file_path, root_dir)
                 lang = "ES" if file.endswith("_ES.md") else "PT" if file.endswith("_PT.md") else None
                 
                 titles = get_title(file_path, file_type)
                 
-                # Obtener la ruta del archivo de configuración correspondiente
                 config_file = os.path.splitext(file_path.replace("_ES", "").replace("_PT", ""))[0] + "_CONFIG.json"
                 config_data = read_config_data(config_file)
                 
@@ -55,76 +51,56 @@ def generate_markdown_list(root_dir):
 
                 slug = f"{track or ''}{'-' + skill if skill else ''}{'-' + module if module else ''}-{os.path.splitext(file)[0]}"
 
-                markdown_dict = {
-                    "track": track,
-                    "skill": skill,
-                    "module": module,
-                    "title": titles[0]["title"] if titles else "Sin título",
-                    "type": file_type,
-                    "path": file_path[2:],
-                    "lang": lang if lang else titles[0]["lang"],
-                    "difficulty": config_data.get("difficulty", ""),
-                    "learning": config_data.get("learning", ""),
-                    "time": config_data.get("time", ""),
-                    "directions": config_data.get("directions", {}).get(lang, ""),
-                    "discord_URL": discord_url,
-                    "discord_channel_id": discord_channel_id,
-                    "discord_message_id": discord_message_id,
-                    "slug": slug
-                }
-
-                # Ajustar el tipo y niveles según el archivo README.md
-                path_parts = file_path.split(os.sep)
-                if file_type == "container" and "README" in file:
-                    if len(path_parts) == 2:
-                        markdown_dict["track"] = None
-                        markdown_dict["skill"] = None
-                        markdown_dict["module"] = None
-                        markdown_dict["type"] = "program"
-                    elif len(path_parts) == 3:
-                        markdown_dict["type"] = "program"
-                        markdown_dict["skill"] = path_parts[1]
-                        markdown_dict["module"] = None
-                    elif len(path_parts) == 4:
-                        markdown_dict["type"] = "skill"
-                        markdown_dict["skill"] = path_parts[1]
-                        markdown_dict["module"] = path_parts[2]
-
-                markdown_list.append(markdown_dict)
+                if file_type and titles:
+                    markdown_dict = {
+                        "track": track,
+                        "skill": skill,
+                        "module": module,
+                        "title": titles[0]["title"],
+                        "type": file_type,
+                        "path": file_path[2:],
+                        "lang": lang if lang else titles[0]["lang"],
+                        "difficulty": config_data.get("difficulty", None),
+                        "learning": config_data.get("learning", None),
+                        "time": config_data.get("time", None),
+                        "directions": config_data.get("directions", {}).get(lang, None),
+                        "discord_URL": discord_url,
+                        "discord_channel_id": discord_channel_id,
+                        "discord_message_id": discord_message_id,
+                        "slug": slug
+                    }
+                    markdown_list.append(markdown_dict)
     return markdown_list
-
-
 
 def get_levels(file_path, root_dir):
     parts = os.path.relpath(file_path, root_dir).split(os.sep)
-    if len(parts) == 1:  # Primer nivel
+    if len(parts) == 1:  # Ignorar archivos README en la carpeta raíz
         return None, None, None
-    elif len(parts) == 2:  # Segundo nivel
+    elif len(parts) == 2:  # Primer nivel
         return parts[0], None, None
-    elif len(parts) >= 3:  # Tercer nivel o más profundo
+    elif len(parts) == 3:  # Segundo nivel
+        return parts[0], parts[1], None
+    elif len(parts) == 4:  # Tercer nivel
         return parts[0], parts[1], parts[2]
     return None, None, None
 
-
-def get_file_type(file_path, subdir, file):
-    if "activities" in subdir and file.endswith(".md") and not file.endswith("README_ES.md") and not file.endswith("README_PT.md"):
-        return "activity"
-    if "topics" in subdir and file.endswith(".md") and not file.endswith("README_ES.md") and not file.endswith("README_PT.md"):
-        return "topic"
-    if file.endswith("README_ES.md") or file.endswith("README_PT.md"):
-        return "container"
-    return "module"  # Asumimos que los demás archivos .md son de tipo "module"
-
+def get_file_type(file_path, root_dir):
+    parts = os.path.relpath(file_path, root_dir).split(os.sep)
+    if len(parts) == 2 and parts[1] == "README.md":
+        return "program"
+    elif len(parts) == 3 and parts[2] == "README.md":
+        return "skill"
+    elif len(parts) == 4 and parts[3] == "README.md":
+        return "module"
+    return None
 
 def get_title(file_path, file_type):
     with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
-        title_match = re.search(r'#\s*(.+)', content)  # Busca el primer h1
+        title_match = re.search(r'#\s*(.+)', content)
         if title_match:
-            return [{"title": title_match.group(1).strip(), "lang": "ES" if file_path.endswith("_ES.md") else "PT"}]
-        else:
-            return [{"title": "Sin título", "lang": "ES" if file_path.endswith("_ES.md") else "PT"}]
-
+            return [{"title": title_match.group(1).strip(), "lang": "ES"}]
+        return [{"title": "Sin título", "lang": "ES"}]
 
 def read_config_data(config_file):
     try:
@@ -162,7 +138,6 @@ if __name__ == "__main__":
     save_to_csv(markdown_list, "markdown_files.csv")
     save_to_json(markdown_list, "markdown_files.json")
 
-    # Enviar datos al endpoint
     endpoint_url = "https://us-central1-laboratoria-prologue.cloudfunctions.net/dj-curriculum-get" 
     if endpoint_url:
         send_data_to_endpoint(endpoint_url, markdown_list)
